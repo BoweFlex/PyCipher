6d5a6c7be7723d6ba58183a83ac364fede69862c
Jonathan Bowe <bowe.jonathan@yahoo.com>
2020-02-06 10:08:31 -0500

    Adding main.py file!
    Mostly testing that I know how to commit and push and verifying things
    work. Right now main just prints a welcome message. More changes soon

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("Welcome to Jonathan's Cipher and Encryption program!")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+if __name__ == "__main__":
+    main()
